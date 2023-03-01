01b0c096c302bd178a2215c06ba43d1d0b03789f
adamknapp124 <aknapp124@gmail.com>
2023-03-01 12:10:52 -0800

    Adds hero section

---
+++ the-vault/src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from '../scss/hero.module.scss';
+
+export default function Hero() {
+	return (
+		<main>
+			<div className={styles.hero}>Hero</div>
+		</main>
+	);
+}
